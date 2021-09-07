3e678a49aa4b9d7c34e4a8b1b605d607f704ce15
Minhaz <minhazrabbi93041@gmail.com>
2021-09-07 11:47:44 +0600

    some file added

--- src/components/hall.js
+++
@@ -1,7 +0,0 @@
-export default function Hall(props) {
-    return (
-        <div>
-            <h1 className="hallColor">This is a {props.hallName} hall, located {props.locate.model}</h1>
-        </div>
-    );
-}
